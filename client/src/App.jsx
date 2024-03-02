3f63c14886fa2d8c611ea9debea19fd556af0bbc
usjav <97538462+usjav@users.noreply.github.com>
2024-03-02 19:51:28 +0500

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './styles/App.css'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import axios from 'axios'
-import MainPage from './pages/Main'
 
-
-
-axios.defaults.baseURL= ' http://localhost:8000'
-axios.defaults.withCredentials=true
 function App() {
- 
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
-        <Route path='/main' element={<MainPage/>} />
-        
-      </Routes>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
