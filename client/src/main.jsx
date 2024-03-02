b61d70845a06455a1c8e6bca878d53dc0f9e9d05
Wahab <25100206@lums.edu.pk>
2024-03-02 22:07:56 +0500

    Sign up page design

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/index.css'
-import {BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Signup from "./pages/Signup.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Signup />
+    </React.StrictMode>
+);
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,121 @@
+import Grid from "@mui/joy/Grid";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
+import logo from "../assets/logo.png";
+import Button from "@mui/joy/Button";
+import Checkbox from "@mui/joy/Checkbox";
+import Divider from "@mui/joy/Divider";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel, { formLabelClasses } from "@mui/joy/FormLabel";
+import Link from "@mui/joy/Link";
+import Input from "@mui/joy/Input";
+import Stack from "@mui/joy/Stack";
+import Radio from "@mui/joy/Radio";
+import FormHelperText from "@mui/joy/FormHelperText";
+
+const Signup = () => {
+    return (
+        <Grid container sx={{ flexGrow: 1, minHeight: "100vh" }}>
+            <Grid
+                item
+                xs={6}
+                sx={{
+                    p: 4,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Grid
+                    sx={{
+                        mb: 4,
+                        display: "flex",
+                        alignItems: "end",
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                    }}
+                    xs={12}
+                >
+                    <img src={logo} alt="logo" style={{ width: "122px" }} />
+                </Grid>
+                <Box xs={8}>
+                    <Typography level="h1" sx={{ mb: 1 }}>
+                        Create Account
+                    </Typography>
+                    <Typography>
+                        Sign up as a Developer or an Company to get started.
+                    </Typography>
+
+                    <Box>
+                        <form>
+                            <Stack gap={4} sx={{ mt: 4 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={6}>
+                                        <FormControl required>
+                                            <FormLabel>First Name</FormLabel>
+                                            <Input
+                                                type="text"
+                                                name="firstName"
+                                            />
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <FormControl required>
+                                            <FormLabel>Last Name</FormLabel>
+                                            <Input
+                                                type="text"
+                                                name="lastName"
+                                            />
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
+                                <FormControl required>
+                                    <FormLabel>Email</FormLabel>
+                                    <Input type="email" name="email" />
+                                </FormControl>
+                                <FormControl required>
+                                    <FormLabel>Password</FormLabel>
+                                    <Input type="password" name="password" />
+                                </FormControl>
+                                <FormControl>
+                                    <Stack gap={2}>
+                                        <FormLabel>I am a...</FormLabel>
+                                        <Radio
+                                            label="Developer"
+                                            defaultChecked
+                                        />
+                                        <Radio label="Company" />
+                                    </Stack>
+                                </FormControl>
+                                <FormControl>
+                                    <Checkbox
+                                        label="Accept Terms & Conditions."
+                                        variant="soft"
+                                        defaultChecked
+                                    />
+                                    <FormHelperText>
+                                        Please review our terms and conditions
+                                        before signing up.
+                                    </FormHelperText>
+                                </FormControl>
+                                <Button type="submit" fullWidth>
+                                    Sign up
+                                </Button>
+                                <Divider></Divider>
+                                <Typography>
+                                    Already have an account? &nbsp;
+                                    <Link>Log in to your account. </Link>
+                                </Typography>
+                            </Stack>
+                        </form>
+                    </Box>
+                </Box>
+            </Grid>
+            <Grid item xs={6} sx={{ backgroundColor: "#f5f5f5" }}>
+                {/*Background column */}
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Signup;
