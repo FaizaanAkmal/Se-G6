bc66f76272639b260cf273ba438edcfa3c772861
usjav <97538462+usjav@users.noreply.github.com>
2024-03-03 23:02:10 +0500

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
-// import Page1 from './pages/onboarding/company/Page1.jsx';
+import Page1 from './pages/onboarding/company/Page1.jsx';
 // import Page2 from './pages/onboarding/company/Page2.jsx';
 // import Page3 from './pages/onboarding/company/Page3.jsx';
 // import Page1 from './pages/onboarding/dev/Page1.jsx';
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         {/* <Route path="/login" element={<Login />} />
         <Route path="/" element={<Signup />} /> */}
-        {/* <Route path='/' element={<Page1 />} /> */}
+        <Route path='/' element={<Page1 />} />
         {/* <Route path='/' element={<Page3 />} /> */}
       </Routes>
     </BrowserRouter>
