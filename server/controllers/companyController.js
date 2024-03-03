6bd0515edcf652f6238ac6c66d4275add0525d38
faisalnazir67 <faisalnazeer67@gmail.com>
2024-03-03 23:40:43 +0500

    Squashed commit of the following:
    commit 365ce4e571ad4d456644f7ab6f6f88e3cfa0bbdb
    Author: FaizaanAkmal <faizaan.akmal@gmail.com>
    Date:   Sun Mar 3 23:00:33 2024 +0500
    
        Dev Onboarding Backend
    
    commit 0943c967b6c088dc1025a6c119efc6d0749fed1c
    Author: FaizaanAkmal <faizaan.akmal@gmail.com>
    Date:   Sun Mar 3 22:17:20 2024 +0500
    
        Company onboarding backend
    
    commit a1893c342fcc1ba7fd30060f75966c7276f68d64
    Author: FaizaanAkmal <faizaan.akmal@gmail.com>
    Date:   Sun Mar 3 19:31:36 2024 +0500
    
        backend schema
    
    commit 8530c4bc01916368fdb36f47dca1906287adb329
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 23:38:22 2024 +0500
    
        Updated
    
    commit 982c6772d4919025ca5f7a61f44cfd7015f10c11
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 23:20:47 2024 +0500
    
        Revert "Merge branch 'usama_branch' into wahab-branch"
    
        This reverts commit 7caf2ea1f99d7f21060cb7a19f77dcbf74e85aa0, reversing
        changes made to 39e82acac376d5d66e4a0846cda8025fc3c04885.
    
    commit ff0c2385a8261fabdde3a82bb13e92456f4b5be3
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 23:20:34 2024 +0500
    
        Revert "Merged with usama_branch"
    
        This reverts commit 4d8e4440693f2bfa898b5c3df8f498f7c020293f.
    
    commit 4d8e4440693f2bfa898b5c3df8f498f7c020293f
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 22:44:56 2024 +0500
    
        Merged with usama_branch
    
    commit 7caf2ea1f99d7f21060cb7a19f77dcbf74e85aa0
    Merge: 39e82ac 9d97da6
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 22:44:17 2024 +0500
    
        Merge branch 'usama_branch' into wahab-branch
    
    commit 9d97da640129ade1dfe8cfdc4a7fd49dac72c9c1
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 22:12:25 2024 +0500
    
        updated
    
    commit d858f6790ac0579af5043d62bd8280798f3481c4
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 21:28:16 2024 +0500
    
        created signup
    
    commit 3f63c14886fa2d8c611ea9debea19fd556af0bbc
    Author: usjav <97538462+usjav@users.noreply.github.com>
    Date:   Sat Mar 2 19:51:28 2024 +0500
    
        initial commit

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -29,4 +29,69 @@ const companyRegister = async (req, res) => {
     }
 };
 
-module.exports = companyRegister;
+// const page1 = async (req, res) => {
+//     const { name, website, type, country } = req.body;
+//     const existingName = await Company.findOne({ name });
+//     if (existingName) {
+//     return res.status(400).json({ success: false, message: 'Company name is already registered.' });
+//     }
+//     const existingWebsite = await Company.findOne({ website });
+//     if (existingWebsite) {
+//     return res.status(400).json({ success: false, message: 'Website is already registered.' });
+//     }
+//     Company.industry, Company.size, Company.overview, Company.workCulture, Company.benefits = null
+//     const newUser = new Company({
+//         name,
+//         website,
+//         type,
+//         country
+//         });
+//         await newUser.save();
+
+     
+//         res.status(201).json({ success: true, message: 'User registered successfully.',newUser });
+// }
+
+const page2 = async (req, res) => {
+    const { industry, size} = req.body;
+    const {id} = req.params;
+    try {
+        const updatedCompany = await Company.findOneAndUpdate(
+            { _id: id }, // Filter: Find the company by its ID
+            { industry, size }, // Update: Set the new industry and size
+            { new: true } // Options: Return the updated document
+        );
+
+        // If the company doesn't exist, return 404
+        if (!updatedCompany) {
+            return res.status(404).json({ error: "Company not found" });
+        }
+
+        res.status(200).json(updatedCompany);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+const page3 = async (req, res) => {
+    const { overview, workCulture, benefits} = req.body;
+    const {id} = req.params;
+    try {
+        const updatedCompany = await Company.findOneAndUpdate(
+            { _id: id }, // Filter: Find the company by its ID
+            { overview, workCulture, benefits }, // Update: Set the new industry and size
+            { new: true } // Options: Return the updated document
+        );
+
+        // If the company doesn't exist, return 404
+        if (!updatedCompany) {
+            return res.status(404).json({ error: "Company not found" });
+        }
+
+        res.status(200).json(updatedCompany);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+module.exports = {companyRegister, page2, page3};
--- server/routes/companyRoute.js
+++ server/routes/companyRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const companyRouter = express.Router()
 const cors = require('cors')
-const companyRegister = require('../controllers/companyController')
+const {companyRegister,page2,page3} = require('../controllers/companyController')
 
 
 companyRouter.post('/companyData',companyRegister)
+router.patch('/page2/:id',page2)
+router.patch('/page3/:id',page3)
 
 module.exports = companyRouter
