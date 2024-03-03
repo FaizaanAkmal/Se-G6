b968ad75378204ed84e68dbbeac192054e545800
usjav <97538462+usjav@users.noreply.github.com>
2024-03-03 11:30:38 +0500

    added onboarding page1 (company)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
+import Page1 from './pages/onboarding/company/Page1.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Signup />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Signup />} /> */}
+        <Route path='/' element={<Page1 />} />
       </Routes>
     </BrowserRouter>
   );
