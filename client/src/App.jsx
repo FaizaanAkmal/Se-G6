675c711b79f5e8d7f9c9dc7beab3624a7cc4ec4a
Salman-Jara <152447821+Salman-Jara@users.noreply.github.com>
2024-03-03 21:42:58 +0500

    New Job Post
    It adds a new job post.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,21 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
+import JobPostForm from './pages/createJob.jsx'
 import Page1 from './pages/onboarding/company/Page1.jsx';
 import Page2 from './pages/onboarding/company/Page2.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/login" element={<Login />} />
         <Route path="/" element={<Signup />} /> */}
         {/* <Route path='/' element={<Page1 />} /> */}
+        <Route path='/signup' element = {<Signup />}/>
+        <Route path='/login' element = {<Login />}/>
+        <Route path='/createjob' element = {<JobPostForm />}/>
         <Route path='/' element={<Page2 />} />
       </Routes>
     </BrowserRouter>
