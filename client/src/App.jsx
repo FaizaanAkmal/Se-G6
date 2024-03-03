4b550c9968ab64a59c54aa1ecaf615a43639e15f
faisalnazir67 <faisalnazeer67@gmail.com>
2024-03-04 00:00:13 +0500

    Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import Page3 from './pages/onboarding/company/Page3.jsx';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import JobPostForm from './pages/createJob.jsx'
 import Page1 from './pages/onboarding/company/Page1.jsx';
 import Page2 from './pages/onboarding/company/Page2.jsx';
--- server/routes/companyRoute.js
+++ server/routes/companyRoute.js
@@ -5,7 +5,7 @@ const {companyRegister,page2,page3} = require('../controllers/companyController'
 
 
 companyRouter.post('/companyData',companyRegister)
-router.patch('/page2/:id',page2)
-router.patch('/page3/:id',page3)
+companyRouter.patch('/page2/:id',page2)
+companyRouter.patch('/page3/:id',page3)
 
 module.exports = companyRouter
