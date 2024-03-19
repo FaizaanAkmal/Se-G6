d0a605cba22d04b22b86da8aee01bcc901f0c9f6
Wahab <25100206@lums.edu.pk>
2024-03-19 14:22:51 +0500

    Refactored Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,6 +16,8 @@ import Stack from "@mui/joy/Stack";
 import Radio from "@mui/joy/Radio";
 import FormHelperText from "@mui/joy/FormHelperText";
 import RadioGroup from "@mui/joy/RadioGroup";
+import loginBackground from "../assets/loginBackground.png";
+import Alert from "@mui/joy/Alert";
 
 export default function Login() {
     const [email, setEmail] = useState("");
@@ -49,17 +51,18 @@ export default function Login() {
                 item
                 xs={6}
                 sx={{
-                    p: 4,
+                    p: 0,
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
+                    // maxHeight: "100vh",
                 }}
             >
                 <Grid
                     sx={{
                         mb: 4,
                         display: "flex",
-                        alignItems: "end",
+                        alignItems: "space-between",
                         flexDirection: "row",
                         justifyContent: "flex-start",
                     }}
@@ -78,22 +81,50 @@ export default function Login() {
                             <Stack gap={4} sx={{ mt: 4 }}>
                                 <FormControl required>
                                     <FormLabel>Email</FormLabel>
-                                    <Input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                    <Input
+                                        type="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
                                 </FormControl>
                                 <FormControl required>
                                     <FormLabel>Password</FormLabel>
-                                    <Input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                                    <Input
+                                        type="password"
+                                        name="password"
+                                        value={password}
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
                                 </FormControl>
-                                <Button type="submit" fullWidth>
-                                    Sign up
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    sx={{
+                                        backgroundColor: "#7F56D9",
+                                        "&:hover": {
+                                            backgroundColor: "#6d4ecb",
+                                        },
+                                    }}
+                                >
+                                    Login
                                 </Button>
+                                {/**Center the error inside the alert */}
+                                {error && (
+                                    <Alert variant="soft" color="danger">
+                                        {error}
+                                    </Alert>
+                                )}
                                 <Divider></Divider>
                                 <Typography>
                                     Don't have an account? &nbsp;
-                                    <Link href="/">
-                                        Sign up to create an account.{" "}
+                                    <Link href="/" sx={{ color: "#7F56D9" }}>
+                                        Sign up to create an account.
                                     </Link>
-                                    {/* How to link? */}
                                 </Typography>
                             </Stack>
                         </form>
@@ -101,7 +132,16 @@ export default function Login() {
                 </Box>
             </Grid>
             <Grid item xs={6} sx={{ backgroundColor: "#f5f5f5" }}>
-                {/*Background column */}
+                {/*Background column, set to loginBackgroun  of available height and width, crop the rest*/}
+                <img
+                    src={loginBackground}
+                    alt="loginBackground"
+                    style={{
+                        width: "100%",
+                        height: "100vh",
+                        objectFit: "cover",
+                    }}
+                />
             </Grid>
         </Grid>
     );
