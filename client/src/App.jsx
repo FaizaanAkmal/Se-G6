3bf209fbe00595ee68578d9e78adc5e169acd77b
usjav <97538462+usjav@users.noreply.github.com>
2025-08-07 03:02:07 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -56,7 +56,7 @@ const newTheme = extendTheme({
   },
 });
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 function App() {
